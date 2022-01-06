1acfbbf90a5611800e0ec90d541f3cba896a162b
IrynaSolonenko <iryna17z89@gmail.com>
2022-01-06 17:40:06 +0200

    commit changes

--- Solonenko12/script.js
+++ Solonenko12/script.js
@@ -2,18 +2,8 @@ let listSub;
 let listLink;
 let arr = [];
 function getBookList(part, chapter, view) {
-<<<<<<< Updated upstream
     if (part) {
         listSub = (document.querySelectorAll('.list')[part-1].getElementsByClassName('list-sub')[chapter - 1]);
-=======
-    let list = document.querySelectorAll('.list');
-    if (part === 1) {
-        listSub = (list[0].getElementsByClassName('list-sub')[chapter - 1]);
-    } else if (part === 2) {
-        listSub = (list[1].getElementsByClassName('list-sub')[chapter - 1]);
-    } else if (part === 3) {
-        listSub = (list[2].getElementsByClassName('list-sub')[chapter - 1]);
->>>>>>> Stashed changes
     } else if (part !== part || !part >= 1 || !part <= 3 || (typeof part) !== 'Number') {
         console.warn('the entered data "part" is incorrect');
     }
@@ -30,23 +20,15 @@ function getBookList(part, chapter, view) {
         } else if (view === 'current') {
             let currentNum = (i + 1) + ' ' + listLink[i].innerText;
             console.log(`${chapter}.${currentNum}`)
+
             arr.push(chapter, currentNum);
         } else if (view !== view || (typeof view) !== 'String') {
             console.warn('the entered data "view" is incorrect')
-        // }  else if (chapter !== chapter || chapter < 0 || chapter > 28 || (typeof chapter) !== 'Number') {
-        //     console.warn('the entered data "chapter" is incorrect');
-<<<<<<< Updated upstream
-        //    возникают проблемы с валидацией данных. Путаюсь куда ее вставлять
+            // }  else if (chapter !== chapter || chapter < 0 || chapter > 28 || (typeof chapter) !== 'Number') {
+            //     console.warn('the entered data "chapter" is incorrect');
+            //    возникают проблемы с валидацией данных. Путаюсь куда ее вставлять
         }
     }
     console.table(arr)
 }
 getBookList(1, 1,  'dash')
-=======
-        }
-    }
-
-    console.table(arr)
-}
-    getBookList(1, 1,  'dash')
->>>>>>> Stashed changes
