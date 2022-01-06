399ce246e336507eacc17a3a81912c3b6824a9cd
IrynaSolonenko <iryna17z89@gmail.com>
2022-01-06 16:38:50 +0200

    push change

--- Solonenko12/script.js
+++ Solonenko12/script.js
@@ -2,37 +2,37 @@ let listSub;
 let listLink;
 let arr = [];
 function getBookList(part, chapter, view) {
-    let list1 = document.querySelectorAll('.list')[0];
-    let list2 = document.querySelectorAll('.list')[1];
-    let list3 = document.querySelectorAll('.list')[2];
+    let list = document.querySelectorAll('.list');
     if (part === 1) {
-        listSub = (list1.getElementsByClassName('list-sub')[chapter - 1]);
+        listSub = (list[0].getElementsByClassName('list-sub')[chapter - 1]);
     } else if (part === 2) {
-        listSub = (list2.getElementsByClassName('list-sub')[chapter - 1]);
+        listSub = (list[1].getElementsByClassName('list-sub')[chapter - 1]);
     } else if (part === 3) {
-        listSub = (list3.getElementsByClassName('list-sub')[chapter - 1]);
+        listSub = (list[2].getElementsByClassName('list-sub')[chapter - 1]);
+    } else if (part !== part || !part >= 1 || !part <= 3 || (typeof part) !== 'Number') {
+        console.warn('the entered data "part" is incorrect');
     }
     listLink = listSub.querySelectorAll('.list-sub__link');
-    if (view === 'dash') {
-        for (let elem of listLink) {
+    for (let i = 0; i < listLink.length; i += 1) {
+        if (view === 'dash') {
+            let elem = listLink[i];
             console.log(`${'-'} ${elem.innerText}`);
             arr.push(`${'-'} ${elem.innerText}`);
-        }
-        console.table(arr)
-    } else if(view === 'number') {
-        for ( let i=0; i < listLink.length; i += 1) {
-           let numberNum = (i+1) + '. ' + listLink[i].innerText;
+        } else if (view === 'number') {
+            let numberNum = (i + 1) + '. ' + listLink[i].innerText;
             console.log(numberNum);
             arr.push(numberNum);
-        }
-        console.table(arr)
-        } else if (view === 'current'){
-        for ( let i=0; i < listLink.length; i += 1) {
-            let currentNum = (i + 1) +' '+ listLink[i].innerText;
+        } else if (view === 'current') {
+            let currentNum = (i + 1) + ' ' + listLink[i].innerText;
             console.log(`${chapter}.${currentNum}`)
-            a.push(chapter, currentNum)
+            arr.push(chapter, currentNum);
+        } else if (view !== view || (typeof view) !== 'String') {
+            console.warn('the entered data "view" is incorrect')
+        }  else if (chapter !== chapter || chapter < 0 || chapter > 28 || (typeof chapter) !== 'Number') {
+            console.warn('the entered data "chapter" is incorrect');
         }
-        console.table(arr)
     }
+
+    console.table(arr)
 }
-getBookList(1, 3, 'number')
+getBookList(17, 1,  'dash')
