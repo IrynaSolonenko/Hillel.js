9d822cc6175c0629a2f349602b1665bd985626c8
IrynaSolonenko <iryna17z89@gmail.com>
2022-01-18 12:00:39 +0200

    commit new hw14

--- Solonenko14/folding-list/script.js
+++ Solonenko14/folding-list/script.js
@@ -1,42 +1,15 @@
-// let listenedBlocks = document.querySelectorAll('.color');
-// let listenedBlock;
-//
-// listenedBlocks.forEach(listenedBlock => {
-//         listenedBlock.addEventListener('click', cont);
-//
-// });
-// function cont(event) {
-//     if (event.target.className === 'color') {
-//         event.target.classList.toggle('show')
-//     }
-//
-//     let outerBlock;
-//
-//     if (event.target.classList.contains('color')) {
-//         outerBlock = event.target;
-//     } else {
-//         outerBlock = event.target.closest('.color');
-//     }
-//     if (!outerBlock) return;
-//     let innerBlock = outerBlock.querySelector('[data-color]');
-//     innerBlock.classList.toggle('show');
-    // listenedBlock.removeEventListener("click", cont)
-// }
+let accButtons = document.querySelectorAll('.accordion');
+let info = document.querySelectorAll('.info');
 
-
-let listenedBlocks = document.querySelectorAll('.block');
-let current = document.querySelectorAll('.color');
-
-listenedBlocks.forEach((listenedBlock)=>{
-    listenedBlock.addEventListener('click',(event)=>{
-        current.forEach((elem)=> {
-            if (event.target.closest('.color')) {
-                event.target.classList.toggle('show');
-                elem.classList.remove('show');
-            } else {
-                event.classList.remove('show');
-            }
+accButtons.forEach(button=>{
+    button.addEventListener('click',function(){
+        if (this.nextElementSibling.classList.contains('show')){
+            this.nextElementSibling.classList.remove('show');
+            return;
+        }
+        info.forEach(inf=>{
+            inf.classList.remove('show');
         })
-    });
-
+        this.nextElementSibling.classList.toggle('show');
+    })
 })
