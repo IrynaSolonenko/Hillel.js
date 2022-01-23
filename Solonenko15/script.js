d244bdff7d667407ec34a9bec9b378cc69992d9b
IrynaSolonenko <iryna17z89@gmail.com>
2022-01-23 20:38:40 +0200

    update hw15

--- Solonenko15/script.js
+++ Solonenko15/script.js
@@ -8,8 +8,9 @@ let amountTime;
 let sumMandarin;
 let lastScoreInfo;
 let lastScoreInfoArr = [];
-let mandarinBestScore = document.querySelector('.mandarinBestScore');
+let mandarinLastScore = document.querySelector('.mandarinLastScore');
 let restartButton = document.querySelector('#restartButton');
+let restartBestResult = document.querySelector('#restartBestResult');
 
 
 let mandarinsAmount = 0;
@@ -17,7 +18,8 @@ let gameDuration = 5;
 let gameInterval;
 let mandarinInterval;
 let grinchIntervalDelay;
-
+let bestScore;
+let bestScoreInfoArr;
 
 
 icon.addEventListener('click', event=>{
@@ -98,29 +100,48 @@ restartButton.addEventListener('click', event => {
     clearInterval(mandarinInterval);
     addlastScoreInfo();
 });
-let bestScore;
-function addlastScoreInfo(){
+
+function addlastScoreInfo() {
     //Создаем табличку с последними данными
     lastScoreInfo = {
         time: amountTime,
         amount: sumMandarin
     }
     lastScoreInfoArr.push(lastScoreInfo);
-    lastScoreInfoArr.forEach(amounts=>{
-        lastScoreInfoArr.sort(amounts = (a, b) => a.amount < b.amount ? 1 : -1);
-    })
 
-    if (lastScoreInfoArr.length > 1){
-        lastScoreInfoArr.splice(2, lastScoreInfoArr.length);
-        console.log(lastScoreInfoArr);
+    if (lastScoreInfoArr.length > 1) {
+        lastScoreInfoArr.splice(2, lastScoreInfoArr.length - 1);
     }
 
+
+    //добавляем последние результаты в Last Score
     for (let key in lastScoreInfoArr) {
         let info = document.createElement('tr');
-        if (typeof sumMandarin === 'undefined'){
+        if (typeof sumMandarin === 'undefined') {
             sumMandarin = 0;
         }
-        info.innerHTML = `<td>${amountTime}</td><td>${sumMandarin}</td>`;
-        mandarinBestScore.append(info);
+        if (typeof amountTime === 'undefined') {
+            amountTime = 0;
+        }
+        info.innerHTML += `<td style="padding: 8px;">${amountTime}</td><td style="padding: 8px;">${sumMandarin}</td>`;
+        mandarinLastScore.innerHTML = '';
+        mandarinLastScore.append(info);
     }
+}
+
+restartBestResult.addEventListener('click', bestScoreArr)
+function bestScoreArr(){
+    //добавляем результаты в Best Score
+    lastScoreInfoArr.forEach(amounts => {
+        lastScoreInfoArr.sort(amounts = (a, b) => a.amount < b.amount ? 1 : -1);
+    })
+    bestScoreInfoArr = lastScoreInfoArr.slice()
+    for (let key in bestScoreInfoArr) {
+        let best = document.createElement('tr');
+        best.innerHTML += `<td style="padding: 8px;">${key.time}</td><td style="padding: 8px;">${key.amount}</td>`;
+        mandarinBestScore.innerHTML = '';
+        mandarinBestScore.append(best);
+    }
+
+    console.log(bestScoreInfoArr)
 }
